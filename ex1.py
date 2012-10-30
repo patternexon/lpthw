adf87937865da606d9dfc91f36d97e4838b786c9
Akshat Shrivastava <patternexon@gmail.com>
2012-10-29 20:27:49 -0400

    First 2 exercises of Learn Python the hard way

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print "Hello World"
+print "Hello World!"
+print "I am typing this"
+print "This is fun."
+print "Yay! Printing."
+#print "I'd rather you 'not'."
+print 'I "said" do not touch this'
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Learning simple things like 
+# commeting 
+
+print "THis is just a simple line" # and uncommeting
+
+# Learning slowly 
+# may not the most smart thing to do 
+
+print " And this is another line"
