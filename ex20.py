a60a19045080f16a68e8644feaf1d775e0d322ca
Akshat Shrivastava <patternexon@gmail.com>
2012-12-04 00:49:02 -0500

    some more changes

--- ex20.py
+++ ex20.py
@@ -17,11 +17,19 @@ print "First let's print the whole file:\n"
 
 print_all(current_file)
 
+<<<<<<< HEAD
 print "Now let's print the whole file:\n"
 
 rewind(current_file)
 
 print "Let's print three lines:"
+=======
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print the three lines:"
+>>>>>>> origin/master
 
 current_line = 1
 print_a_line(current_line, current_file)
@@ -32,4 +40,7 @@ print_a_line(current_line, current_file)
 current_line = current_line + 1
 print_a_line(current_line, current_file)
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> origin/master
